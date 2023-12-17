3e9c4dd5ea2e1501b244e583ed7c13a86606db2f
keskin1i <121793800+keskin1i@users.noreply.github.com>
2023-12-17 16:44:18 +0300

    ürünlerin tablosu oluşturuldu

---
+++ product-service/src/main/java/com/product_service/dto/Product.java
@@ -0,0 +1,54 @@
+package com.product_service.dto;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "stock")
+    private int stock;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
