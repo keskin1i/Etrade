03ffc441cff2ba8287789db4fb3b47e35e33409b
keskin1i <121793800+keskin1i@users.noreply.github.com>
2023-12-17 15:22:05 +0300

    eklendi...

---
+++ euroka service/euroka-service/src/main/java/com/euroka_service/EurokaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.euroka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurokaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurokaServiceApplication.class, args);
+	}
+
+}
---
+++ euroka service/euroka-service/src/test/java/com/euroka_service/EurokaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euroka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurokaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
